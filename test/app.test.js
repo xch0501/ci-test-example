74cc2b872b49df0f5d9ad638a15470823c777679
UChuuEki <UChuuEki@noreply.gitcode.com>
2025-04-03 16:52:02 +0800

    Add CI configuration

--- test/app.test.js
+++ test/app.test.js
@@ -1,6 +1,6 @@
 const add = require('../app');
 const { expect } = require('chai');
-
+import chai from 'chai';
 describe('Add function', () => {
     it('should return 5 when adding 2 and 3', () => {
         expect(add(2, 3)).to.equal(5);
